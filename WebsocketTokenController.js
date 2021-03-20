cf23e7237824f6bb268ac66bec42b5af4659e797
shawly <shawlyde@gmail.com>
2021-03-20 14:16:03 +0100

    Added settings menu. Added some comments about token control

--- WebsocketTokenController.js
+++ WebsocketTokenController.js
@@ -1,13 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Constants
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let socket;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
 // Hooks
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,43 +8,59 @@ let socket;
  * Init hook
  */
 Hooks.once("init", () => {
-    // write init code (e.g. register settings for module menu)
-    console.log("Loading WebsocketTokenController Module.");
 
-    /*game.settings.registerMenu("websocket-token-controller", "websocketHost", {
-        name: "WSTKNCTRL.websocketHost",
-        label: "WSTKNCTRL.websocketHostTitle",
-        hint: "WSTKNCTRL.websocketHostHint",
+    game.settings.register("websocket-token-controller", "enabled", {
+        scope: "world",
+        type: Boolean,
+        default: true,
+        config: false
+    })
+
+    game.settings.register("websocket-token-controller", "websocketHost", {
+        name: "WebsocketTokenController.websocketHost",
+        hint: "WebsocketTokenController.websocketHostHint",
         scope: "world",
         type: String,
-        default: "192.168.0.8",
+        default: "node-red.home.viromania.com",
         config: true,
         onChange: () => {
-            location.reload();
+            location.reload()
         }
-    });
+    })
 
-    game.settings.registerMenu("websocket-token-controller", "websocketPort", {
-        name: "WSTKNCTRL.websocketPort",
-        label: "WSTKNCTRL.websocketPortTitle",
-        hint: "WSTKNCTRL.websocketPortHint",
+    game.settings.register("websocket-token-controller", "websocketPort", {
+        name: "WebsocketTokenController.websocketPort",
+        hint: "WebsocketTokenController.websocketPortHint",
         scope: "world",
         type: String,
-        default: "1880",
+        default: "443",
         config: true,
         onChange: () => {
-            location.reload();
+            location.reload()
         }
-    });*/
-});
+    })
+
+    game.settings.registerMenu("websocket-token-controller", "websocket-token-controller", {
+        name: "WebsocketTokenController.config",
+        label: "WebsocketTokenController.configTitle",
+        hint: "WebsocketTokenController.configHint",
+        icon: "fas fa-keyboard",
+        type: TokenControllerConfig,
+        restricted: false
+    })
+
+    console.log("WebsocketTokenController | Loaded settings")
+})
 
 /**
  * Ready hook
  */
 Hooks.once("ready", () => {
-    console.log("Initializing WebsocketTokenController Module.");
-    game.wstokenctrl = new TokenController();
-});
+    if (game.settings.get("websocket-token-controller", "enabled")) {
+        console.log("WebsocketTokenController | Starting websocket connection")
+        game.wstokenctrl = new TokenController()
+    }
+})
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -79,8 +87,8 @@ export class TokenController {
      * Constructor. Initialize websocketTokenController.
      */
     constructor() {
-        Hooks.call("websocketTokenControllerInit", this);
-        this._initializeWebsocket();
+        Hooks.call("websocketTokenControllerInit", this)
+        this._initializeWebsocket()
     }
 
     /**
@@ -89,44 +97,81 @@ export class TokenController {
      * @private
      */
     _initializeWebsocket() {
-        const $this = this;
-        let wsInterval; // Interval timer to detect disconnections
-        let ip = "node-red.home.viromania.com"; // game.settings.get("websocket-token-controller", "websocketHost");
-        let port = "443"; //game.settings.get("websocket-token-controller", "websocketPort");
-        socket = new WebSocket("wss://" + ip + ":" + port + "/ws/vtt");
+        const $this = this
+        let wsInterval // Interval timer to detect disconnections
+        let ip = game.settings.get("websocket-token-controller", "websocketHost")
+        let port = game.settings.get("websocket-token-controller", "websocketPort")
+        let socket = new WebSocket("wss://" + ip + ":" + port + "/ws/vtt")
 
         socket.onmessage = function (message) {
-            console.log("Token Controller: received message");
+            console.log("WebsocketTokenController | Received message: ")
             const data = JSON.parse(message.data)
-            console.dir(data);
-            $this._handleMovement(data);
+            console.dir(data)
+            $this._handleMovement(data)
         }
 
         socket.onopen = function () {
-            ui.notifications.info("Token Controller: " + game.i18n.localize("WebsocketTokenController.Notifications.Connected") + ip + ":" + port);
+            ui.notifications.info("Websocket Token Controller: " + game.i18n.localize("WebsocketTokenController.Notifications.Connected") + ip + ":" + port)
             // do stuff
+        }
+
+        socket.onerror = function (error) {
+            ui.notifications.error("Websocket Token Controller: " + game.i18n.localize("WebsocketTokenController.Notifications.Error"))
+            console.error("WebsocketTokenController | Error: ", error)
         }
     }
 
     _handleMovement(message) {
-        if ( !message.key ) return;
-        if ( message.state != 'down') return;
-    
+        if (!message.key) return
+        if (message.state != 'down') return
+
         // Get controlled objects
         //TODO: find currently selected token for the keyboard that sent the command
-    
+
         // Define movement offsets and get moved directions
-        const directions = message.key.split('');
-        let dx = 0;
-        let dy = 0;
-    
+        const directions = message.key.split('')
+        let dx = 0
+        let dy = 0
+
         // Assign movement offsets
-        if ( directions.includes('W') ) dx -= 1;
-        if ( directions.includes('S') ) dy += 1;
-        if ( directions.includes('E') ) dx += 1;
-        if ( directions.includes('N') ) dy -= 1;
-    
+        if (directions.includes('N')) dy -= 1
+        if (directions.includes('E')) dx += 1
+        if (directions.includes('S')) dy += 1
+        if (directions.includes('W')) dx -= 1
+
         // Perform the shift or rotation
-        canvas.tokens.moveMany({dx, dy});
-      }
+        canvas.tokens.moveMany({ dx, dy })
+    }
+}
+
+/**
+ * Form application to assign keyboards to players.
+ */
+class TokenControllerConfig extends FormApplication {
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            title: game.i18n.localize("WebsocketTokenController.configTitle"),
+            id: "websocket-token-controller-config",
+            template: "/home/ths/Projects/FoundryVTT-Websocket-Token-Controller/chrome-overrides/modules/websocket-token-controller/templates/keyboard-config.html",
+            width: 500,
+            height: "auto",
+            closeOnSubmit: true,
+            tabs: [{ navSelector: ".tabs", contentSelector: "form", initial: "general" }]
+        })
+    }
+
+    // we can get users via "game.users.entities" (returns all users) or "game.users.players" (returns only non-GM users)
+
+    // check if a token can be controlled by a player example:
+    // var timo = game.users.entities[1]
+    // var alex = game.users.entities[2]
+    // var thilando = canvas.tokens.objects.children[0]
+    // var vlad = canvas.tokens.objects.children[1]
+    // thilando.can(timo, "control") = true
+    // thilando.can(alex, "control") = false
+    // vlad.can(alex, "control") = true
+    // vlad.can(timo, "control") = false
+    //
+    // if timo is gm it will always return true
 }
